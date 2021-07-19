b90eb9b57450d4974bbad77ccf1f7ec3f2082930
Kalooz <mkaluzny17@gmail.com>
2021-07-19 12:37:03 -0500

    Fixed Party Section and Added Mobile Nav Menu

--- main.js
+++ main.js
@@ -1,57 +1,47 @@
-(function($) {
+(function ($) {
   "use strict"; // Start of use strict
 
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 3500,
+    autoplayHoverPause: true,
 
-$('.owl-carousel').owlCarousel({
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
 
-  loop:true,
-  nav:false,
-  dots: false,
-  autoplay:true,
-  autoplayTimeout:3500,
-  autoplayHoverPause:true,
-
-  responsive:{
-    0:{
-        items:1
+      2500: {
+        items: 2,
+      },
     },
-    600:{
-        items:1
-    },
-
-    2500:{
-      items:2
-    }
-    
-}
-})
+  });
 
   // Collapse Navbar
-  var navbarCollapse = function() {
+  var navbarCollapse = function () {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-scrolled");
-
     } else {
       $("#mainNav").removeClass("navbar-scrolled");
-
     }
   };
 
-    $("li.list-group-item").click(function(){
-      $("li.list-group-item").removeClass("active");
-      $(this).addClass("active");
-      $("#priceIMG").attr('src',$(this).data("img"));
-      $("#text").text($(this).data("service"));
-      $("#price").text($(this).data("price"));
-    })
- 
+  $("li.service").click(function () {
+    $("li.service").removeClass("active");
+    $(this).addClass("active");
+    $("#priceIMG").attr("src", $(this).data("img"));
+    $("#text").text($(this).data("service"));
+    $("#price").text($(this).data("price"));
+  });
+
   // Collapse now if page is not at top
   navbarCollapse();
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
-
-
-
 })(jQuery); // End of use strict
-
-  
