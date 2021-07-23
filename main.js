b15947d5b2ad681eccfcb264354a439027cb72c4
Kalooz <mkaluzny17@gmail.com>
2021-07-22 20:00:32 -0500

    Resized Photos

--- main.js
+++ main.js
@@ -35,9 +35,12 @@
   $("li.service").click(function () {
     $("li.service").removeClass("active");
     $(this).addClass("active");
+
     $("#priceIMG").attr("src", $(this).data("img"));
+    $("#title").text(this.textContent);
     $("#text").text($(this).data("service"));
     $("#price").text($(this).data("price"));
+    $("#description").text($(this).data("description"));
   });
 
   // Collapse now if page is not at top
