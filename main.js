e540af2991e2a41a330f99c5eed6210c5ed0fc22
Kalooz <mkaluzny17@gmail.com>
2021-08-11 15:52:35 -0500

    Presentation
    Updated accesability

--- main.js
+++ main.js
@@ -1,27 +1,7 @@
+AOS.init();
+
 (function ($) {
   "use strict"; // Start of use strict
-
-  $(".owl-carousel").owlCarousel({
-    loop: true,
-    nav: false,
-    dots: false,
-    autoplay: true,
-    autoplayTimeout: 3500,
-    autoplayHoverPause: true,
-
-    responsive: {
-      0: {
-        items: 1,
-      },
-      600: {
-        items: 1,
-      },
-
-      2500: {
-        items: 2,
-      },
-    },
-  });
 
   // Collapse Navbar
   var navbarCollapse = function () {
---
+++ owlcarousel.js
@@ -0,0 +1,25 @@
+(function ($) {
+  "use strict"; // Start of use strict
+
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 3500,
+    autoplayHoverPause: true,
+
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+
+      2500: {
+        items: 2,
+      },
+    },
+  });
+})(jQuery); // End of use strict
