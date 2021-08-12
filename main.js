1075ec26fbffe21ce7dfab1561a28559381d2698
Kalooz <mkaluzny17@gmail.com>
2021-08-12 13:02:54 -0500

    JavaScript Edits

--- main.js
+++ main.js
@@ -2,27 +2,6 @@ AOS.init();
 
 (function ($) {
   "use strict";
-  $(".owl-carousel").owlCarousel({
-    loop: true,
-    nav: false,
-    dots: false,
-    autoplay: true,
-    autoplayTimeout: 3500,
-    autoplayHoverPause: true,
-
-    responsive: {
-      0: {
-        items: 1,
-      },
-      600: {
-        items: 1,
-      },
-
-      2500: {
-        items: 2,
-      },
-    },
-  });
 
   var navbarCollapse = function () {
     if ($("#mainNav").offset().top > 100) {
